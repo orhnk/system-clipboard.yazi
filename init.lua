9b32493c2b7cc7b40f39a3252882d990b73687db
orhnk <utfeightt@gmail.com>
2024-08-14 22:42:27 +0300

    update: use "the clipboard" instead of xclip

--- init.lua
+++ init.lua
@@ -1,21 +1,54 @@
--- Meant to run at sync context. (yazi --sync simple-clipboard)
+-- Meant to run at async context. (yazi system-clipboard)
+
+local selected_or_hovered = ya.sync(function()
+	local tab, paths = cx.active, {}
+	for _, u in pairs(tab.selected) do
+		paths[#paths + 1] = tostring(u)
+	end
+	if #paths == 0 and tab.current.hovered then
+		paths[1] = tostring(tab.current.hovered.url)
+	end
+	return paths
+end)
 
 return {
 	entry = function()
 		ya.manager_emit("escape", { visual = true })
 
-		local files = cx.active.current.hovered
-		local xclip = {
-			"xclip",
-			"-selection",
-			"clipboard",
-			"-i",
-			"-t",
-			files:mime(),
-			"<",
-			tostring(files.url),
-		}
+		local urls = selected_or_hovered()
 
-		os.execute(table.concat(xclip, " "))
+		if #urls == 0 then
+			return ya.notify({ title = "System Clipboard", content = "No file selected", level = "warn", timeout = 5 })
+		end
+
+		-- ya.notify({ title = #urls, content = table.concat(urls, " "), level = "info", timeout = 5 })
+
+		local status, err =
+				Command("cb")
+				:arg("copy")
+				:args(urls)
+				:spawn()
+				:wait()
+
+		if status or status.succes then
+			ya.notify({
+				title = "System Clipboard",
+				content = "Succesfully copied the file(s) to system clipboard",
+				level = "info",
+				timeout = 5,
+			})
+		end
+
+		if not status or not status.success then
+			ya.notify({
+				title = "System Clipboard",
+				content = string.format(
+					"Could not copy selected file(s) %s",
+					status and status.code or err
+				),
+				level = "error",
+				timeout = 5,
+			})
+		end
 	end,
 }
